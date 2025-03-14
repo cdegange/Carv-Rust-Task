c3a71fe9b53498a4e78eabf9f37e70e506e12cbf
Chad DeGange <chaddegange@gmail.com>
2025-03-14 16:37:30 -0600

    init: Implement main.rs core logic structure

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ mod github;
 mod cache;
 mod utils;
 
+use crate::cli::parse_args;
+use crate::github::fetch_repos;
+use crate::utils::{display_repos, save_repos};
 
 fn main() {
-    println!("Hello, world!");
+    if args = parse_args();
+
+    let all_repos = fetch_repos(&args);
+
+    if let Some(path) = &args.save_path {
+        save_repos(&all_repos, path);
+    } else {
+        display_repos(&all_repos);
+    }
 }
